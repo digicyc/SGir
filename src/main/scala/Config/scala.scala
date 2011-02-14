159e61f30e1073798c20ca4c9676ce270e945f20
Aaron A <digicyc@gmail.com>
2011-02-14 11:13:43 -0700

    Lots of conversions over to using IDEA.

---
+++ src/main/scala/Config/scala.scala
@@ -0,0 +1,13 @@
+package Config
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: aaron
+ * Date: 2/14/11
+ * Time: 11:11 AM
+ * To change this template use File | Settings | File Templates.
+ */
+
+class scala {
+
+}
