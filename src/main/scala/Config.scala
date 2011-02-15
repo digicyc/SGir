cb162d5ca1cf6a44d515f30016c82d2af93bc3d8
Aaron A <digicyc@gmail.com>
2011-02-14 17:11:29 -0700

    Added a crap load of XMPie commands into SGir.

---
+++ src/main/scala/Config.scala
@@ -0,0 +1,20 @@
+/**
+ * Created by IntelliJ IDEA.
+ * User: aaron
+ * Date: 2/14/11
+ * Time: 11:14 AM
+ * To change this template use File | Settings | File Templates.
+ */
+
+import java.util.Properties
+
+
+object Config  {
+  private var props: Properties
+  private var conf = null
+  private val configFile = "etc/sgir.conf"
+
+  def reloadConfig = {
+
+  }
+}
