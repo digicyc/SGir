eb62465eb277fd96ca34e480b29456752d703892
Aaron A <digicyc@gmail.com>
2011-02-16 15:44:14 -0700

    omg major cleaning.

--- project/build/SGir.scala
+++
@@ -1,6 +0,0 @@
-import sbt._
-
-class SGirProject(info: ProjectInfo) extends DefaultProject(info) with IdeaProject {
-  lazy val hi = task { println("SGIR!"); None }
-  val casbah = "com.mongodb.casbah" %% "casbah" % "2.0.2"
-}
