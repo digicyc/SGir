a079785efa4af02325d8dde2e090ac2c5c92988b
Aaron A <digicyc@gmail.com>
2011-02-15 22:43:54 -0700

    Adding in the users object.

--- src/main/scala/Config.scala
+++ src/main/scala/Config.scala
@@ -1,16 +1,15 @@
 /**
- * Created by IntelliJ IDEA.
  * User: aaron
  * Date: 2/14/11
  * Time: 11:14 AM
- * To change this template use File | Settings | File Templates.
+ *
  */
+package org.antitech.sgir
 
 import java.util.Properties
 
 
 object Config  {
-  private var props: Properties
   private var conf = null
   private val configFile = "etc/sgir.conf"
 
---
+++ src/main/scala/Users.scala
@@ -0,0 +1,20 @@
+/**
+ * User: aaron
+ * Date: 2/15/11
+ * Time: 4:55 PM
+ *
+ * Manages users through a mongodb database.
+ */
+import com.mongodb.casbah.Imports._
+
+class Users {
+  private val mongoColl = MongoConnection()("sgir")("users")
+
+  def getOP(user: String) = {
+    val userOp = mongoColl.findOne(MongoDBObject("user" -> user)).foreach {
+      x =>
+      x.get("is_op")
+    }
+    //userOp.get("is_op")
+  }
+}
