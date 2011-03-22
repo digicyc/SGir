ace744cdbed49b895fde5305c6de38bfc0480deb
digicyc <digicyc@gmail.com>
2011-03-21 19:17:51 -0600

    Adding more MongoDB support.

--- src/main/scala/Users.scala
+++ src/main/scala/Users.scala
@@ -5,21 +5,41 @@
  *
  * Manages users through a mongodb database.
  */
+package org.antitech.sgir
 
 import com.mongodb.casbah.Imports._
 
 class Users {
+  private val config = Config.config
   private val mongConn = MongoConnection("hazmat.cc")("sgir")
-
+  /*
+   * Return TimeStamp and Message that was last said by user.
+   */
   def getLastSaid(user: String): String = {
     val mongoColl = mongConn("last_said")
-    val lastSaid = mongoColl.find(MongoDBObject("user" -> user))
+    //val lastSaid = mongoColl.find(MongoDBObject("user" -> user))
 
-    for { x <- lastSaid } yield x
+    ""
   }
 
   def saveLastSaid(user: String, msg: String) {
     val mongoColl = mongConn("last_said")
-    mongoColl.save(MongoDBObject(""))
+    //mongoColl.save(MongoDBObject(""))
+  }
+
+  def addHate(user: String, level: Int) {
+
+  }
+
+  def getHate(user: String): Int = {
+    0
+  }
+
+  def addPoint(user: String, point: Int) {
+
+  }
+
+  def getPoint(user: String): Int = {
+    0
   }
 }
