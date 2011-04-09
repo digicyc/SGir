0f79869e294dccabcbebb655a674d022a17ce0fa
AaronA <digicyc@gmail.com>
2011-04-08 18:55:18 -0600

    More privacy on settings.

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -2,9 +2,8 @@ package org.antitech.sgir
 
 object Main {
     def main(args: Array[String]): Unit = {
-      val config = Config.config
       val sGir = new SGir()
-      sGir.connect(config.getString("ircServer").get)
-      config.getList("channels").foreach{chan => sGir.joinChannel(chan)}
+      sGir.connect(Config.config("ircServer"))
+      Config.config.getList("channels").foreach(sGir.joinChannel(_))
     }
 }
