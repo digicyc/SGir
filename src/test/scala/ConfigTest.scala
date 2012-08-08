8874c176a632e809237cc60d65a6c021a4af357c
AaronA <digicyc@gmail.com>
2012-08-08 11:14:30 -0600

    Adding test for new Config method from typesafe.

---
+++ src/test/scala/ConfigTest.scala
@@ -0,0 +1,12 @@
+package codeoptimus.sgir.test
+
+import org.specs2.mutable._
+
+class ConfigTest extends Specification {
+
+  "Config file path" should {
+    "be pointed correctly" in {
+      
+    }
+  }
+}
